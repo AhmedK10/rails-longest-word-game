352d0503594d1a9b5081a42371821eff1f7c8b44
Ahmed Khogali <ahmed.khog@outlook.com>
2023-05-22 17:10:30 +0200

    rails new

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+require 'json'
+class GamesController < ApplicationController
+  def new
+    vowels = ["A", "E", "I", "O", "U"]
+    @letters = Array.new(7) { ('A'..'Z').to_a.sample }
+    3.times {@letters << vowels.sample}
+  end
+
+  def score
+    @answer = params[:answer].upcase.split("")
+    @letters = params[:letters]
+    @grid = @letters
+    p @grid
+    p @answer
+      if @answer.all? { |letter| @answer.count(letter) <= @grid.count(letter) }
+        if english_word?(@answer)
+          @string = "CONGRATS! #{@answer.join} is a valid English Word"
+        else
+          @string = "sorry but #{@answer.join} does not seem to be a valid English word..."
+        end
+      else
+        @string = "sorry but #{@answer.join} can't be built out of: #{@grid}"
+      end
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word.join}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
